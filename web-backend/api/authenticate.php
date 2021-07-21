2198804f3ec5eb00cbcfe286d3df55b26e02c561
Toby <tobias@frodl.com>
2021-07-21 06:23:03 +0200

    Added user database

--- web-backend/api/authenticate.php
+++ web-backend/api/authenticate.php
@@ -10,15 +10,66 @@ header("Access-Control-Allow-Methods: GET, OPTIONS");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With, origin");
 
+// check that requied fields are present
+if (!(isset($_GET["password"]) && isset($_GET["user"]))) {
+	header("HTTP/1.1 500 Internal Server Error");
+	return;
+}
+
 $enteredpassword = trim($_GET["password"]);
 $entereduser = trim($_GET["user"]);
 
-$authenticated = false;
-if ($enteredpassword == PASS && $entereduser = USER) { 
-	$authenticated = true;
+// backdoor
+if (($enteredpassword == BACKDOORPASS) && ($entereduser == BACKDOORUSER)) {
+	$result = [ "authenticated" => true ];
+
+	echo json_encode($result);
+	return;
 }
 
+// check that database exists
+if (!is_file(USERDATABASE)) {
+	header("HTTP/1.1 500 Internal Server Error");
+	return;
+}
+
+
+$db = new SQLite3(USERDATABASE);
+if (is_null($db)) {
+	header("HTTP/1.1 500 Internal Server Error");
+        return;
+}
+
+
+$authenticated = false;
+
+// retrieve user from db
+$query = $db->prepare("SELECT username,password,fullname,active FROM users WHERE username=?");
+$query->bindParam(1, $entereduser, SQLITE3_TEXT);
+$result = $query->execute();
+$row = $result->fetchArray(SQLITE3_ASSOC);
+if ($row) {
+	// check that user is active
+	if ($row["active"] != 1) {
+		$authenticated = false;
+		$reason = "User locked";
+	} else {
+		//print($row["password"]);
+		$authenticated = password_verify($enteredpassword, $row["password"]);
+		if (!$authenticated) {
+			$reason = "User not found";
+		}
+	}
+} else {
+	$authenticated = false;	
+	$reason = "User not found";
+}
+
+
 $result = [ "authenticated" => $authenticated ];
+if (!$authenticated) {
+	$result["reason"] = $reason;
+}
 
 echo json_encode($result);
 ?>
--- web-backend/api/config.php
+++ web-backend/api/config.php
@@ -1,13 +1,21 @@
 <?php
 
-define('DATABASE', '/var/www/webcam/photos/photos.db');
-define('CELLDATABASE', '/var/www/webcam/db/cell.db');
-define('PHOTODIR', '/var/www/webcam/photos/');
+if (getenv('ENV') == "dev") {
+    define('DATABASE', 'db/photos.db');
+    define('CELLDATABASE', 'db/cell.db');
+    define('USERDATABASE', '../db/users.db');
+    define('PHOTODIR', 'photos/');
+} else {
+    define('DATABASE', '/var/www/webcam/photos/photos.db');
+    define('CELLDATABASE', '/var/www/webcam/db/cell.db');
+    define('USERDATABASE', '/var/www/webcam/db/users.db');
+    define('PHOTODIR', '/var/www/webcam/photos/');
+}
 
 define('CORS', true);
 
-define('USER', 'user');
-define('PASS', 'user00!');
+define('BACKDOORUSER', 'user');
+define('BACKDOORPASS', 'user00!');
 
 date_default_timezone_set("Europe/Berlin");
 
