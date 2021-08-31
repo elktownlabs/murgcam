97ba0e184f12aa92cf50ce54b85d738deaa58b39
Tobias Frodl <tofr@MG.intern>
2021-08-31 14:47:58 +0100

    Vue frontend:  Many changes. Nothing is completely done yet. Made forms more usable; New form for update frequency; Multi-user-mode added

--- web-if/src/App.vue
+++ web-if/src/App.vue
@@ -1,10 +1,82 @@
 <template>
   <v-app>
     <v-app-bar dense app v-if="this.$store.getters.isAuthenticated">
-      <v-app-bar-title><v-icon class="mx-3">mdi-camera-wireless</v-icon>Remote Webcam</v-app-bar-title>
+      <v-app-bar-title><v-icon class="mx-3">mdi-camera-wireless</v-icon>MurgCam</v-app-bar-title>
       <v-spacer/>
-      <v-btn to="/settings" class="mx-1" dark color="indigo"><v-icon>mdi-cogs</v-icon><span class="d-none d-md-block">Settings</span></v-btn>      
-      <v-btn @click="logout" class="mx-1" dark color="indigo"><v-icon>mdi-logout</v-icon><span class="d-none d-md-block">Logout</span></v-btn>
+      <v-btn to="/photos" class="mx-1 hidden-sm-and-down" dark color="primary"><v-icon>mdi-camera</v-icon><span>Photos</span></v-btn>      
+      <v-btn to="/frequency" class="mx-1 hidden-sm-and-down" dark color="primary"><v-icon>mdi-clock-fast</v-icon><span>Photo Freq.</span></v-btn>
+      <v-btn to="/settings" class="mx-1 hidden-sm-and-down" dark color="primary"><v-icon>mdi-cogs</v-icon><span>Settings</span></v-btn>
+      <div class="mx-3"></div>
+      <v-menu transition="slide-y-transition" bottom>
+        <template v-slot:activator="{ on, attrs }">
+            <v-btn class="mx-1 hidden-sm-and-down" icon v-bind="attrs" v-on="on">
+              <v-avatar color="secondary lighten-2 white--text" size="36">TF</v-avatar>
+            </v-btn>
+        </template>
+        <v-card elevation="2" class="mx-auto">
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title class="text-h6 mb-1">Tobias Frodl</v-list-item-title>
+              <v-list-item-subtitle>toby</v-list-item-subtitle>
+            </v-list-item-content>
+            <v-list-item-avatar size="60" color="grey"></v-list-item-avatar>
+          </v-list-item>
+          <v-card-actions>
+            <v-btn outlined rounded @click="logout">
+              <v-icon class="mx-1">mdi-logout</v-icon>Logout
+            </v-btn>
+          </v-card-actions>
+
+        </v-card>
+      </v-menu>
+      <v-menu transition="slide-y-transition" bottom>
+        <template v-slot:activator="{ on, attrs }">
+            <v-btn class="mx-1 hidden-md-and-up" icon v-bind="attrs" v-on="on">
+              <v-icon>mdi-menu</v-icon>
+            </v-btn>
+        </template>
+        <v-list>
+          <v-list-item class="primary lighten-2">
+            <v-list-item-avatar>
+              <img src="https://cdn.vuetifyjs.com/images/john.jpg" alt="John">
+            </v-list-item-avatar>
+            <v-list-item-content>
+              <v-list-item-title>toby</v-list-item-title>
+              <v-list-item-subtitle>Tobias Frodl</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+          <v-divider></v-divider>
+          <v-list-item to="/photos">
+            <v-list-item-icon><v-icon>mdi-camera</v-icon></v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>Photos</v-list-item-title>
+              <v-list-item-subtitle>Library of taken photos</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item to="/frequency">
+            <v-list-item-icon><v-icon>mdi-clock-fast</v-icon></v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>Photo Frequency</v-list-item-title>
+              <v-list-item-subtitle>Temporarily increase photo frequency</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item to="/settings">
+            <v-list-item-icon><v-icon>mdi-cogs</v-icon></v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>Camera Settings</v-list-item-title>
+              <v-list-item-subtitle>Change operational settings</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item @click="logout">
+            <v-list-item-icon><v-icon>mdi-logout</v-icon></v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>Logout</v-list-item-title>
+              <v-list-item-subtitle>Quit session</v-list-item-subtitle>
+             </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+
     </v-app-bar>
     <v-main>
       <router-view></router-view>
