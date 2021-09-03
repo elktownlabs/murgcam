43b8e2659f6170cbd55a8b747406dfcee11ce8b2
Toby <tobias@frodl.com>
2021-09-03 23:21:24 +0200

    Backend: Added config.php

---
+++ web-backend/api/config.php
@@ -0,0 +1,20 @@
+<?php
+
+define('DATABASE', '/var/www/dev-wwv-schwarzwald/webcam/photos/photos.db');
+define('APPDATABASE', '/var/www/dev-wwv-schwarzwald/webcam/db/app.db');
+define('CELLDATABASE', '/var/www/dev-wwv-schwarzwald/webcam/db/cell.db');
+define('PHOTODIR', '/var/www/dev-wwv-schwarzwald/webcam/photos/');
+
+define('CORS', true);
+
+define('MASTERUSER', 'admin');
+define('MASTERPASS', 'dufgypl00!');
+
+define('PUBLICTITLEFONT', '/var/www/dev-wwv-schwarzwald/webcam/api/CascadiaMono-SemiBold.otf');
+define('PUBLICTITLE', 'Murg oberhalb Hundsbachmündung');
+define('PUBLICSUBTITLE', 'Wildwasserverein Schwarzwald e.V.');
+define('PUBLICIMGLOCATION', '/var/www/dev-wwv-schwarzwald/webcam/murgcam.jpg');
+
+date_default_timezone_set("Europe/Berlin");
+
+?>
